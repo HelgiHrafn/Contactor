1395258d51e64a3a96e63c72ec7f167ac75c8eb3
HelgiHrafn <helgia21@ru.is>
2022-11-28 13:30:53 +0000

    contactlist

---
+++ Contactor/src/components/ContactListItem/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './styles'
+
+const ContactListItem = ({ contact }) => {
+    return (
+        <View>
+            <Text>Name</Text>
+            <Text>phoneNumber</Text>
+            <Image></Image>
+        </View>
+    )
+}
+
+export default ContactListItem
