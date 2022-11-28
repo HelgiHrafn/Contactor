03f55e5694f617987de05e85f271ae62b1f7a7e3
HelgiHrafn <helgia21@ru.is>
2022-11-28 15:37:53 +0000

    contacts+

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -3,9 +3,11 @@ import React from 'react'
 import { View, FlatList, Text } from 'react-native'
 import styles from './styles'
 import ContactListItem from '../ContactListItem'
+import { headings } from '../../styles/headings'
 
 const ContactList = ({ contacts, navigation }) => (
-    <View>
+    <View style={styles.list}>
+        <Text style={[headings.h1, styles.title]}>My Contacts</Text>
         <FlatList
             numColumns={1}
             data={contacts.sort((a, b) => a.name.localeCompare(b.name))}
---
+++ src/components/ContactListItem/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+export default StyleSheet.create({
+    contactItem: {
+        borderColor: 'lightgray',
+        borderTopWidth: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 5
+    },
+    image: {
+        width: 30,
+        height: 30,
+        borderRadius: 100,
+        marginRight: 10
+    }
+})
