15bb295b1bcc1b955d0de6d7208ae061f986cdcb
HelgiHrafn <helgia21@ru.is>
2022-11-29 12:07:52 +0000

    refactoring

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -6,48 +6,14 @@ import {View} from 'react-native'
 import AddContactModal from '../../components/AddContactModal'
 import Searchbar from '../../components/SearchBar'
 import * as imageService from '../../services/imageService'
+import tempdata from '../../resources/tempdata.json'
 
 const Contacts = ({ navigation }) => {
 
   // A boolean flag to indicate wether the modal to add a contact is open or not  
   const [isAddModalOpen, setIsAddModalOpen] = useState(false)
 
-  const [contactsMaster, setContactsMaster] = useState([
-      {
-          "id": 1,
-          "name": "Johnny Knoxville",
-          "phoneNumber": "772-4254",
-          "thumbnailPhoto": "https://flxt.tmsimg.com/assets/198743_v9_bb.jpg"
-      },
-      {
-          "id": 2,
-          "name": "Steve O",
-          "phoneNumber": "644-7205",
-          "thumbnailPhoto": "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492113764/articles/2016/04/12/up-close-and-personal-with-the-all-new-steve-o/160411-yamato-steve-o-tease_i0c7sr"
-      },
-      {
-          "id": 3,
-          "name": "Bam Margera",
-          "phoneNumber": "656-9649",
-          "thumbnailPhoto": "https://static.wikia.nocookie.net/fan-hardcore/images/5/50/Bam_margera.jpg/revision/latest?cb=20220213043836"
-      },
-      {
-          "id": 4,
-          "name": "Chris Pontius",
-          "phoneNumber": "538-6449",
-          "thumbnailPhoto": "https://biographyline.com/wp-content/uploads/2021/05/Chris-Pontius.jpg" },
-      {
-            "id": 5,
-            "name": "Dua Lipa",
-            "phoneNumber": "4206969",
-            "thumbnailPhoto": "https://i.insider.com/63713995951bdc00182ddd13?width=700"
-      },
-      {
-            "id": 6,
-            "name": "Saul Goodman",
-            "phone": "(505) 503-4455",
-            "thumbnailPhoto": "https://static.wikia.nocookie.net/inconsistently-heinous/images/e/e0/Saul_2009.jpg/revision/latest?cb=20220828160305"
-      }])
+  const [contactsMaster, setContactsMaster] = useState(tempdata.data)
 
   const [filteredContacts, setFilteredContacts] = useState(contactsMaster)
 
@@ -68,9 +34,6 @@ const Contacts = ({ navigation }) => {
     console.log("We need to add contact to fileservice")
 
     }
-     
-    console.log('master: ', contactsMaster)
-    console.log('filter: ', filteredContacts)
       const filter = (text) =>{
             if (text) {
                   const filtered = contactsMaster.filter(
